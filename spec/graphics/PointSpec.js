e0e924a3c82be53d097d07564233d08ca268f9a4
unknown <danger.dan_@hotmail.co.uk>
2015-07-14 12:35:44 +0100

    Add tests for desired constructor functionality in PointSpec

--- spec/graphics/PointSpec.js
+++ spec/graphics/PointSpec.js
@@ -2,12 +2,57 @@ describe("Point", function() {
 
   var Point = require('../../src/graphics/Point');
 
-  beforeEach(function() {
-    this.pt = Point();
+  it("should default x & y to 0 when no arguments passed to constructor", function() {
+    var pt = Point();
+    expect(pt.x).toEqual(0);
+    expect(pt.y).toEqual(0);
   });
 
-  it("should be ", function() {
-    expect(this.pt.x).toEqual(0);
-    expect(this.pt.y).toEqual(0);
+  it("should take arguments Point(x, y) in constructor", function() {
+    var pt = Point(10, 20);
+    expect(pt.x).toEqual(10);
+    expect(pt.y).toEqual(20);
   });
+
+  it("should take array of length 2 as Point([x, y]) in constructor", function(){
+    var pt = Point([10, 20]);
+    expect(pt.x).toEqual(10);
+    expect(pt.y).toEqual(20);
+  });
+
+  it("should throw exception when passed too short an array", function() {
+
+    expect(function () {
+      var ptTooShortArray = Point([10]);
+    }).toThrow(
+      new Error('Point was passed invalid arguments')
+    );
+  });
+
+  it("should throw exception when passed too long an array", function() {
+
+    expect(function () {
+      var ptTooLongArray = Point([10, 20, 30]);
+    }).toThrow(
+      new Error('Point was passed invalid arguments')
+    );
+  });
+
+  it("should throw exception when not passed enough arguments", function() {
+
+    expect(function () {
+      var ptNotEnoughArgs = Point(10);
+    }).toThrow(
+      new Error('Point was passed invalid arguments')
+    );
+  });
+
+  it("should throw exception when passed too many arguments", function() {
+    expect(function(){
+      var ptTooManyArgs = Point(10, 20, 30);
+    }).toThrow(
+      new Error('Point was passed invalid arguments')
+    );
+  });
+
 });
