3e3c4e286748eaf2036d7def5ccc3559df105912
Dan Brooks <danbroooks@live.co.uk>
2015-07-14 20:16:12 +0100

    Add code to make tests pass

--- src/graphics/Point.js
+++ src/graphics/Point.js
@@ -6,7 +6,35 @@ var Point = function(x, y) {
   this.y = y;
 };
 
-module.exports = function(x, y){
+Point.prototype.shift = function(x, y){
+
+  if (_.isNumber(x) && _.isNumber(y)) {
+    this.x += x;
+    this.y += y;
+  } else if (x instanceof Point && _.isUndefined(y)) {
+    this.x += x.x;
+    this.y += x.y;
+  } else {
+    throw Error('Invalid arguments passed to point.shift');
+  }
+
+  return this;
+};
+
+Point.prototype.invert = function(){
+
+  if (this.x !== 0) {
+    this.x *= -1;
+  }
+
+  if (this.y !== 0) {
+    this.y *= -1;
+  }
+
+  return this;
+};
+
+var Factory = function(x, y){
 
   if (arguments.length > 2) {
     throw Error('Point was passed invalid arguments');
@@ -35,3 +63,23 @@ module.exports = function(x, y){
   }
 
 };
+
+Factory.Constructor = Point;
+
+Factory.Clone = function(inst) {
+  if (inst instanceof Point) {
+    return Factory(inst.x, inst.y);
+  } else {
+    throw Error('Point.Clone must be passed an instance of Point');
+  }
+}
+
+Factory.Add = function(a, b) {
+  return Factory.Clone(a).shift(b);
+};
+
+Factory.Invert = function(point) {
+  return Factory.Clone(point).invert();
+};
+
+module.exports = Factory;
