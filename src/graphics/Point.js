91a14c9927a103332cad82eea2ec422be9900cdd
unknown <danger.dan_@hotmail.co.uk>
2015-07-14 13:03:39 +0100

    Write Point constructor to pass tests

--- src/graphics/Point.js
+++ src/graphics/Point.js
@@ -1,9 +1,37 @@
 
-var Point = function(opts) {
-  this.x = 0;
-  this.y = 0;
+var _ = require('lodash');
+
+var Point = function(x, y) {
+  this.x = x;
+  this.y = y;
 };
 
-module.exports = function(opts){
-  return new Point(opts)
+module.exports = function(x, y){
+
+  if (arguments.length > 2) {
+    throw Error('Point was passed invalid arguments');
+  }
+
+  if (_.isNumber(x) && _.isUndefined(y)) {
+    throw Error('Point was passed invalid arguments');
+  }
+
+  if (!y && _.isArray(x)) {
+
+    if (x.length != 2) {
+      throw Error('Point was passed invalid arguments');
+    }
+
+    return new Point(x[0], x[1]);
+
+  } else if (_.isNumber(x) && _.isNumber(y)) {
+
+    return new Point(x, y);
+
+  } else {
+
+    return new Point(0, 0);
+
+  }
+
 };
